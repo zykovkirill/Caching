f9d9b4a5ce279af46b59816d594c868c98e50539
Zka95 <zykovkirill95@mail.ru>
2023-01-28 17:32:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => services.AddMemoryCache())
+    .Build();
+
+IMemoryCache  memoryCache = host.Services.GetRequiredService<IMemoryCache>();
+
+const int MillisecondDelayAfterAdd = 50;
+const int MillisecondAbsoluteExpiration = 750;
+
+
+static void OnPostEviction(object key, object? letter, EvictionReason reason, object? state)
+{
+    if(letter is AlphabetLetter alphabetLetter)
+    {
+        Console.WriteLine($"{alphabetLetter.Letter} was evicted for {reason}.");
+    }
+}
+
+
+static async ValueTask IterateAlphabetAsync(Func<char, Task> asyncFunc)
+{
+    for(char letter = 'A'; letter <= 'Z'; ++letter)// letter++)
+    {
+        await asyncFunc(letter);
+    }
+    Console.WriteLine();
+}
+
+var addLettersToCacheTask = IterateAlphabetAsync(letter =>
+{
+    MemoryCacheEntryOptions options = new()
+    {
+        AbsoluteExpirationRelativeToNow = TimeSpan.FromMilliseconds(MillisecondAbsoluteExpiration)
+    };
+
+    _ = options.RegisterPostEvictionCallback(OnPostEviction);
+
+    AlphabetLetter alphabetLetter = memoryCache.Set(letter, new AlphabetLetter(letter), options);
+
+    Console.WriteLine($"{alphabetLetter.Letter} was cached.");
+
+    return Task.Delay(TimeSpan.FromMilliseconds(MillisecondDelayAfterAdd));
+
+});
+
+await addLettersToCacheTask;
+
+var readLettersFromCacheTask = IterateAlphabetAsync(letter =>
+{
+    if (memoryCache.TryGetValue(letter, out object? value) && value is AlphabetLetter alphabetLetter)
+    {
+        Console.WriteLine($"{letter} is still in cahe. {alphabetLetter.Message}");
+    }
+
+    return Task.CompletedTask;
+
+});
+
+await readLettersFromCacheTask;
+await host.RunAsync();
+record AlphabetLetter(char Letter)
+{
+    internal string Message =>
+        $"The '{Letter}' character is the {Letter - 64} letter in the English alphabet.";
+}
